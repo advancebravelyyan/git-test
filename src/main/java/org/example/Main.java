0c2dfee917cce19ba872822c242d444f79b2287f
liyan <liyan@wuhan.com>
2023-05-15 20:21:09 +0800

    hot-fix commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Java");
+        System.out.println("后端工程师");
         System.out.println("全栈工程师");
     }
 }
